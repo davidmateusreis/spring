26fb7e312a341c0a322286fc08c07be2a7d8cc65
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-06-26 10:11:13 -0300

    Aula 04
    Persistindo dados obtidos por meio de um formulário

---
+++ biblioteca/src/main/java/com/david/biblioteca/controllers/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.david.biblioteca.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.david.biblioteca.models.Usuario;
+import com.david.biblioteca.repositories.UsuarioRepository;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/novo")
+	public String adicionarUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "/publica-criar-usuario";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvarUsuario(@Valid Usuario usuario, BindingResult result, 
+				RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "/publica-criar-usuario";
+		}	
+		usuarioRepository.save(usuario);
+		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
+		return "redirect:/usuario/novo";
+	}
+
+}
---
+++ biblioteca/src/main/java/com/david/biblioteca/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.david.biblioteca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.david.biblioteca.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
